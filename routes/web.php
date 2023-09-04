0a591294698f256933f821f122188c6b0ffc10fb
rojhelat <omercicek01@gmail.com>
2023-09-04 19:52:03 +0300

    ucuncu gün

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AnasayfaController;
+use App\Http\Controllers\KategoriController;
+use  App\Http\Controllers\UrunController;
+use \App\Http\Controllers\SepetController;
+use \App\Http\Controllers\KullaniciController;
+use \App\Http\Controllers\SiparisController;
+use \App\Http\Controllers\SiparislerController;
+use \App\Http\Controllers\OdemeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +20,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('anasayfa');
-});
+Route::get('/',[AnasayfaController::class,'index'])->name('anasayfa');
+Route::get('/kategori',[KategoriController::class,'index'])->name('kategori');
+Route::get('/urun',[UrunController::class,'index'])->name('urun');
+Route::get('/sepet',[SepetController::class,'index'])->name('sepet');
+Route::get('/odeme',[OdemeController::class,'index'])->name('odeme');
+Route::get('/siparis',[SiparisController::class,'index'])->name('siparis');
+Route::get('/siparisler',[SiparislerController::class,'index'])->name('siparisler');
+Route::get('/kullanici/giris',[KullaniciController::class,'giris'])->name('kullanici_giris');
+Route::get('/kullanici/kayit',[KullaniciController::class,'kayit'])->name('kullanici_kayit');
+
